ca526ac53055471ec45f4972adaeb63fb744c9b3
kevin ikahu <kevinikahu4@gmail.com>
2023-04-16 10:01:27 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+const taskInput = document.getElementById("task-input");
+const submitButton = document.getElementById("submit-button");
+const taskList = document.getElementById("task-items");
+
+submitButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  const task = taskInput.value;
+  if (task !== "") {
+    const taskItem = document.createElement("li");
+    taskItem.innerText = task;
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+  }
+});
